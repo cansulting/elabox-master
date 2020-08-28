1762fd90924f7a9d4007efdd9d5aba1c9e8136c8
ademcan <ademcan@ademcan.net>
2020-08-28 21:54:13 +0200

    Remove promise from control_fan.js

--- control_fan.js
+++ control_fan.js
@@ -6,22 +6,22 @@ const fanOut = new Gpio('4', 'out');
 
 
 
-const fanControl = () => {
-    return new Promise((resolve, reject) => {
+// const fanControl = () => {
+//     return new Promise((resolve, reject) => {
         exec('cat /sys/class/thermal/thermal_zone0/temp', { maxBuffer: 1024 * 500 }, (err, stdout, stderr) => {
             console.log(stdout)
             if (stdout > 70000) {
                 console.log("Starting fan")
                 fanOut.writeSync(1)
-                resolve()
+                // resolve()
             }
             else {
                 console.log("Stopping fan")
                 fanOut.writeSync(0)
-                resolve()
+                // resolve()
             }
         });
-    })
-}
+//     })
+// }
 
-exports = fanControl
+// exports = fanControl
