3e78e45eeb3fe4f398a77b952fabf39c4e70ab32
Purujit Bansal <thegeekybaniya@users.noreply.github.com>
2020-09-11 17:45:45 +0000

    Updated cron

--- startup_npmi.sh
+++ startup_npmi.sh
@@ -4,17 +4,20 @@ echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ STARTING BOOT TIME CRON ~~~~~~~~~~~~~~~~~~~~
 cd /home/elabox/elabox-master/
 echo "Current Directory: `pwd`, Installing Modules for Master" >> /home/elabox/elabox-master/startup_logs.log
 echo "Starting Time : `date`" >> /home/elabox/elabox-master/startup_logs.log
-npm install >> /home/elabox/elabox-master/startup_logs.log
+echo "elabox" | sudo -S npm install >> /home/elabox/elabox-master/startup_logs.log
+echo "elabox" | sudo -S npm rebuild
 echo "Ending Time: `date`" >> /home/elabox/elabox-master/startup_logs.log
 cd /home/elabox/companion/
 echo "Current Directory: `pwd`, Installing Modules for Companion" >> /home/elabox/elabox-master/startup_logs.log
 echo "Starting Time: `date`" >> /home/elabox/elabox-master/startup_logs.log
-npm install >> /home/elabox/elabox-master/startup_logs.log
+echo "elabox" | sudo -S npm install >> /home/elabox/elabox-master/startup_logs.log
+echo "elabox" | sudo -S npm rebuild
 echo "Ending Time: `date`" >> /home/elabox/elabox-master/startup_logs.log
 cd /home/elabox/elabox-binaries/
 echo "Current Directory: `pwd`, Installing Modules for Binaries Repo" >> /home/elabox/elabox-master/startup_logs.log
 echo "Starting Time: `date`" >> /home/elabox/elabox-master/startup_logs.log
-npm install >> /home/elabox/elabox-master/startup_logs.log
+echo "elabox" | sudo -S npm install >> /home/elabox/elabox-master/startup_logs.log
+echo "elabox" | sudo -S npm rebuild
 echo "Ending Time: `date`" >> /home/elabox/elabox-master/startup_logs.log
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ENDING BOOT TIME CRON ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" >> /home/elabox/elabox-master/startup_logs.log
 
